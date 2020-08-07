29d97dacceac141459010d292528cb3e52a0b143
Andy Sommerville <aksommerville@gmail.com>
2020-08-07 19:50:05 -0400

    Identity management

--- src/server/main.js
+++ src/server/main.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 const { guessMimeType } = require("./mime.js");
+const userService = require("./userService.js");
 
 const serverInterface = process.env["INTF"] || "localhost";
 const serverPort = +(process.env["PORT"] || "8080");
@@ -12,26 +13,67 @@ function logTransaction(request, response) {
   console.log(`${new Date().toISOString()} ${response.statusCode} ${request.method} ${request.url}`);
 }
 
+function cannedErrorMessage(statusCode) {
+  switch (statusCode) {
+    case 401: return "Unauthorized";
+    case 403: return "Forbidden";
+    case 404: return "Not Found";
+    case 405: return "Method Not Allowed";
+    case 599: return "TODO";
+  }
+  switch (Math.floor(statusCode / 100)) {
+    case 200: return "OK";
+    case 300: return "Redirect";
+    case 400: return "Client Error";
+    case 500: return "Internal Server Error";
+  }
+  return "";
+}
+
 function serveError(request, response, error) {
-  if (!error) error = {};
+  if (typeof(error) === "number") error = { statusCode: error };
+  else if (!error) error = {};
   response.statusCode = error.statusCode || 500;
-  response.status = error.statusMessage || `Internal Server Error`;
+  response.statusMessage = error.statusMessage || cannedErrorMessage(response.statusCode);
   response.end();
   logTransaction(request, response);
 }
 
+function serveVoid(request, response) {
+  response.statusCode = 200;
+  response.statusMessage = "OK";
+  response.end();
+  logTransaction(request, response);
+}
+
+function serveText(request, response, text) {
+  response.statusCode = 200;
+  response.statusMessage = "OK";
+  response.end(text);
+  logTransaction(request, response);
+}
+
+function serveJson(request, response, object) {
+  const json = JSON.stringify(object);
+  response.statusCode = 200;
+  response.statusMessage = "OK";
+  response.end(json);
+  logTransaction(request, response);
+}
+
 function serveStaticFile(request, response, root) {
+  if (request.method !== "GET") {
+    return serveError(request, response, 405);
+  }
   fs.realpath(`${root}/${request.url}`, (error, path) => {
     if (error || !path.startsWith(root)) return serveError(request, response, {
       ...error,
       statusCode: 404,
-      statusMessage: "Not found",
     });
     fs.readFile(path, (error, data) => {
       if (error) return serveError(request, response, {
         ...error,
         statusCode: 404,
-        statusMessage: "Not found",
       });
       
       response.setHeader("Content-Type", guessMimeType(path, data));
@@ -42,17 +84,154 @@ function serveStaticFile(request, response, root) {
   });
 }
 
+/* Remote-control diagnostics.
+ ********************************************************************/
+ 
+function serveSelfTest(request, response) {
+
+  console.log("**** PERFORMING SELF TEST PER REQUEST ****");
+
+  const { users, sessions } = userService.getAll();
+  console.log("All users (permanent list):");
+  for (const user of users) {
+    console.log(`  ${user.id} '${user.name}'`);
+  }
+  console.log("Currently logged in:");
+  for (const session of sessions) {
+    console.log(`  ${session.id} '${session.name}' ${session.expire}`);
+  }
+  
+  console.log("**** END OF SELF TEST ****");
+
+  // Don't tell the user that this service exists.
+  serveError(request, response, 404);
+}
+
+/* /api/player
+ *******************************************************************/
+ 
+function validateUserName(name) {
+  if (!name) return false;
+  return !!name.match(/^[0-9a-zA-Z_\-.]{1,12}$/);
+}
+
+function validateUserId(id) {
+  if (!id) return false;
+  return !!id.match(/^[0-9a-zA-Z_\-.]{1,12}$/);
+}
+
+function validateNewPassword(password) {
+  if (!password) return false;
+  if (password.length < 8) return false;
+  return true;
+}
+ 
+function serveLogin(request, response) {
+  const id = request.urlObject.searchParams.get("id");
+  const password = request.body;
+  const tokenAndName = userService.login(id, password);
+  if (!tokenAndName) return serveError(request, response, 403);
+  return serveJson(request, response, tokenAndName);
+}
+
+function serveNewPlayer(request, response) {
+  const name = request.urlObject.searchParams.get("name");
+  if (!validateUserName(name)) return serveError(request, response, {
+    statusCode: 400,
+    statusMessage: "Invalid user name.",
+  });
+  
+  // (id) is optional. If present, we create a new persistent user record.
+  const id = request.urlObject.searchParams.get("id");
+  if (id) {
+    if (!validateUserId(id)) return serveError(request, response, {
+      statusCode: 400,
+      statusMessage: "Invalid user id.",
+    });
+    const password = request.body;
+    if (!validateNewPassword(password)) return serveError(request, response, {
+      statusCode: 400,
+      statusMessage: "Invalid password.",
+    });
+    if (!userService.createUser(id, password, name)) return serveError(request, response, {
+      statusCode: 400,
+      statusMessage: "User id already taken.",
+    });
+    return serveLogin(request, response);
+  }
+  
+  // Anonymous login...
+  const accessToken = userService.loginWithoutCredentials(name);
+  if (!accessToken) return serveError(request, response, 500);
+  return serveJson(request, response, { accessToken, name });
+}
+
+function serveDeletePlayer(request, response) {
+  userService.deleteUser(request.user.id);
+  return serveVoid(request, response);
+}
+
+function serveLogout(request, response) {
+  userService.logout(request.user.id);
+  return serveVoid(request, response);
+}
+
+/* REST dispatch.
+ ******************************************************************/
+
+function serveApi(request, response) {
+  const url = new URL(`http://example.com${request.url}`);
+  request.urlObject = url;
+  const endpointName = `${request.method} ${url.pathname}`;
+  
+  /* Unathenticated calls.
+   */
+  switch (endpointName) {
+  
+    case "GET /api/time": return serveText(request, response, new Date().toISOString());
+    case "POST /api/player/login": return serveLogin(request, response);
+    case "POST /api/player/new": return serveNewPlayer(request, response);
+    case "POST /api/selftest": return serveSelfTest(request, response);
+  
+  }
+  
+  /* Authenticated calls.
+   */
+  const user = userService.authenticateRequest(request);
+  if (!user) return serveError(request, response, 401);
+  request.user = user;
+  switch (endpointName) {
+  
+    case "DELETE /api/player": return serveDeletePlayer(request, response);
+    case "POST /api/player/logout": return serveLogout(request, response);
+    case "POST /api/room/new": return serveError(request, response, 599);
+    case "GET /api/room": return serveError(request, response, 599);
+    case "DELETE /api/room": return serveError(request, response, 599);
+    case "PUT /api/room": return serveError(request, response, 599);
+    case "POST /api/join": return serveError(request, response, 599);
+    case "POST /api/leave": return serveError(request, response, 599);
+  
+  }
+  serveError(request, response, 404);
+}
+
+/* Service dispatch.
+ ******************************************************************/
+
 function serve(request, response) {
   try {
+  
+    if (request.url === "/") request.url = "/index.html";
     
-    //TODO serve things other than regular files
-    
-    if (htdocsDir) {
-      serveStaticFile(request, response, htdocsDir);
-      return;
+    if (request.url.startsWith("/api/")) {
+      return serveApi(request, response);
     }
     
-    serveError(request, response, { statusCode: 404, statusMessage: "Not found" });
+    if (htdocsDir) {
+      return serveStaticFile(request, response, htdocsDir);
+    }
+    
+    serveError(request, response, 404);
     
   } catch (e) {
     console.error(e);
@@ -71,6 +250,17 @@ server.listen(serverPort, serverInterface, (error) => {
     } else {
       console.log(`Will not serve static files due to HTDOCS unset.`);
     }
-    server.on("request", serve);
+    server.on("request", (request, response) => {
+      let body = "";
+      request.on("data", (data) => (body += data));
+      request.on("end", () => {
+        request.body = body;
+        serve(request, response);
+      });
+    });
   }
 });
+
+setInterval(() => {
+  userService.dropExpiredSessions();
+}, 1000 * 60);
---
+++ src/server/userService.js
@@ -0,0 +1,166 @@
+const crypto = require("crypto");
+
+/* Globals
+ *******************************************************************/
+
+const SESSION_EXPIRE_TIME_MS = 1000 * 60 * 20;
+
+/* Persistent user list. (TODO db or something)
+ * {
+ *   id: string
+ *   hash: sha256 of "ID:PASSWORD"
+ *   name: string
+ * }
+ */
+const users = [];
+
+/* Logged-in users.
+ * {
+ *   accessToken: string
+ *   id: string | undefined
+ *   name: string
+ *   expire: number(time)
+ * }
+ */
+const sessions = [];
+
+/* Access token.
+ *******************************************************************/
+ 
+function generateAccessToken() {
+  return crypto.randomBytes(12).toString("base64");
+}
+
+/* Check authentication for existing session.
+ *******************************************************************/
+
+function authenticateRequest(request) {
+  const authorization = request.headers["authorization"];
+  if (!authorization) return null;
+  const [authMethod, accessToken] = authorization.trim().split(/\s+/);
+  if (authMethod.toLowerCase() !== "bearer") return null;
+  if (!accessToken) return null;
+  
+  for (const session of sessions) {
+    if (session.accessToken !== accessToken) continue;
+    session.expire = Date.now() + SESSION_EXPIRE_TIME_MS;
+    return {
+      id: session.id,
+      name: session.name,
+    };
+  }
+  
+  // Nice try, Ivan.
+  return null;
+}
+
+/* Login: Validate id and password, and create a new session.
+ *****************************************************************/
+ 
+function login(id, password) {
+  if (!id || !password) return null;
+  if (sessions.find(s => s.id === id)) return null; // already logged in
+  const hash = crypto.createHash("sha256").update(`${id}:${password}`).digest("hex");
+  for (const user of users) {
+    if (user.hash === hash) {
+      const accessToken = generateAccessToken();
+      sessions.push({
+        accessToken,
+        id,
+        name: user.name,
+        expire: Date.now() + SESSION_EXPIRE_TIME_MS,
+      });
+      return {
+        accessToken,
+        name: user.name,
+      };
+    }
+  }
+  return null;
+}
+
+/* Login without credentials: Just a made-up name that needn't even be unique.
+ *****************************************************************/
+ 
+function loginWithoutCredentials(name) {
+  const accessToken = generateAccessToken();
+  sessions.push({
+    accessToken,
+    name,
+    expire: Date.now() + SESSION_EXPIRE_TIME_MS,
+  });
+  return accessToken;
+}
+
+/* Create a new user record. Returns boolean.
+ ****************************************************************/
+ 
+function createUser(id, password, name) {
+  if (!id || !password || !name) return false;
+  if (users.find(u => u.id === id)) return false;
+  const hash = crypto.createHash("sha256").update(`${id}:${password}`).digest("hex");
+  users.push({
+    id,
+    hash,
+    name,
+  });
+  return true;
+}
+
+/* Delete a user record, and log him out if logged in.
+ *****************************************************************/
+ 
+function deleteUser(id) {
+  let index = users.findIndex(u => u.id === id);
+  if (index >= 0) users.splice(index, 1);
+  index = sessions.findIndex(s => s.id === id);
+  if (index >= 0) sessions.splice(index, 1);
+}
+
+/* Logout.
+ *****************************************************************/
+ 
+function logout(id) {
+  const index = sessions.findIndex(s => s.id === id);
+  if (index >= 0) {
+    sessions.splice(index, 1);
+    return true;
+  }
+  return false;
+}
+
+/* Return a copy of all users and sessions.
+ *****************************************************************/
+ 
+function getAll() {
+  return {
+    users: users.map(u => ({ ...u })),
+    sessions: sessions.map(s => ({ ...s })),
+  };
+}
+
+/* Drop expired sessions.
+ *****************************************************************/
+ 
+function dropExpiredSessions() {
+  const now = Date.now();
+  for (let i=sessions.length; i-->0; ) {
+    const session = sessions[i];
+    if (session.expire > now) continue;
+    sessions.splice(i, 1);
+  }
+}
+
+/* TOC
+ *****************************************************************/
+
+module.exports = {
+  authenticateRequest,
+  login,
+  createUser,
+  loginWithoutCredentials,
+  deleteUser,
+  logout,
+  getAll,
+  dropExpiredSessions,
+};
